20e707fe97459413c594ac9780596756b9e41cc7
lalithkrishna31 <34646061+lalithkrishna31@users.noreply.github.com>
2017-12-19 12:56:53 +0530

    Create code3.c

---
+++ assignment/code3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() 
+{
+char x;
+scanf("%c",&x);
+if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U')
+{
+    printf("vowel");
+}
+else
+{
+    printf("not vowel");
+}
+}
